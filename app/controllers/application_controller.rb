bc850947147c893765da05cae43931d7f3a524ff
katsu0510 <katsu0510310@gmail.com>
2021-02-08 18:52:57 +0900

    first

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :k_first_name, :k_last_name, :birth_day])
+  end
 end
