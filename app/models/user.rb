e6ec208ab5b86ce5169c4d9f23530d339840ec93
katsu0510 <katsu0510310@gmail.com>
2021-02-10 15:33:02 +0900

    ユーザー管理機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,17 +11,14 @@ class User < ApplicationRecord
   with_options presence: true do
     validates :nickname
     validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
-
     with_options format: {with: /\A[ぁ-んァ-ヶ一-龥]+\z/ } do
     validates :last_name
     validates :first_name
     end
-
     with_options format: {with: /\A[ァ-ヶー－]+\z/} do
     validates :k_last_name
     validates :k_first_name
     end
-
     validates :birth_day
   end
   
