8b209cdad6c1ad5a205930157c83badfe47db250
katsu0510 <katsu0510310@gmail.com>
2021-02-15 16:52:24 +0900

    商品出品機能の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,7 +11,7 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :name
     validates :description
-    validates :price, :numericality => {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}, format: {with: /\A[0-9]+\z/ }
+    validates :price, :numericality => {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
     validates :image
       with_options numericality: { other_than: 0 } do
       validates :category_id
