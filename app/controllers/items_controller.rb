7f5b6456749156994170bc3c607151cf140378a1
katsu0510 <katsu0510310@gmail.com>
2021-02-15 20:06:53 +0900

    商品詳細表示機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
 
   before_action :move_to_index, except: [:index, :show, :new]
   before_action :authenticate_user!, only:[:new, :edit, :destroy ]
- 
+  before_action :set_item, only: [:show]
 
   def index
     @items = Item.order("created_at DESC")
@@ -50,5 +50,9 @@ end
 
 def item_params
   params.require(:item).permit(:name, :description, :category_id, :state_id, :ship_cost_id, :prefecture_id, :ship_day_id, :price, :image).merge(user_id: current_user.id)
+end
+
+def set_item
+  @item = Item.find(params[:id])
 end
 
